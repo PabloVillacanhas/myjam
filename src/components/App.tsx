d7cce00856f07c88b1c413abfa54bc9f7de79202
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2020-12-12 15:12:22 +0000

    APlied theme

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,22 @@
-import * as React from "react";
-import Login from "./Login";
-import { hot } from "react-hot-loader";
+import * as React from 'react';
+import Login from './Login';
+import { Container } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { hot } from 'react-hot-loader';
+import { Alert } from '@material-ui/lab';
+import theme from '../themes/theme';
 
 class App extends React.Component<Record<string, unknown>, undefined> {
-	public render() {
-		return (
-			<div className="app">
-				<Login />
-			</div>
-		);
-	}
+  public render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          <Alert severity="error">This is an error alert — check it out!</Alert>
+          <Login />
+        </Container>
+      </ThemeProvider>
+    );
+  }
 }
 
 declare let module: Record<string, unknown>;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,13 +5,20 @@ import {
 	FormHelperText,
 	Input,
 	InputLabel,
-	Button,
+  Button,
+  Card,
+  CardContent,
+  ThemeProvider,
+  useTheme,
 } from "@material-ui/core";
 import { useState } from "react";
 import { postSession } from "../api/session";
 import { Session } from "../types/types";
 
-const Login: React.FC<{}> = () => {
+interface Props{}
+
+const Login: React.FC<Props> = () => {
+
 	const [formValues, setFormValues] = useState<Session>(undefined);
 
 	const onChangeInput = (e) => {
@@ -19,29 +26,32 @@ const Login: React.FC<{}> = () => {
 	};
 
 	return (
-		<Container maxWidth="md">
-			<FormControl>
-				<InputLabel htmlFor="code">Session code</InputLabel>
-				<Input
-					id="code"
-					aria-describedby="code_h"
-					onChange={(e) => onChangeInput(e)}
-				/>
-				<FormHelperText id="code_h">
-					Create a new session
-				</FormHelperText>
-			</FormControl>
-			<Button
-				variant="contained"
-				color="primary"
-				onClick={() => {
-					postSession(formValues)
-				}}
-			>
-				Create the session
-			</Button>
-      {JSON.stringify(formValues)}
-		</Container>
+    <Container maxWidth="md">
+      <Card>
+        <CardContent className="hola">
+        <FormControl>
+          <InputLabel htmlFor="code">Session code</InputLabel>
+          <Input
+            id="code"
+            aria-describedby="code_h"
+            onChange={(e) => onChangeInput(e)}
+          />
+          <FormHelperText id="code_h">
+            Create a new session
+          </FormHelperText>
+        </FormControl>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              postSession(formValues)
+            }}
+          >
+            Create the session
+          </Button>
+        </CardContent>
+      </Card> 
+    </Container>
 	);
 };
 
