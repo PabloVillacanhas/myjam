85ac0c1e5fb57bce755cd2adafe0821347d2c4ce
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2020-12-12 00:36:36 +0000

    Seteado en torno, comprendido entorno

--- express.js
+++ express.js
@@ -1,15 +1,17 @@
+const path = require('path')
 const express = require('express');
 const app = express();
 const portNumber = 3000;
 const sourceDir = 'dist';
 
 app.use(express.static(sourceDir));
+app.set('views', path.join(__dirname, '/src/views'));
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
   console.log(`Serving content from /${sourceDir}/`);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World, from express');
+app.get('/about', function (req, res) {
+  res.send('about');
 });
