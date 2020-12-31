7217e04fdbfc8f28f10ae5f3dcf3f748554d66ee
Pablo Dominguez Villacanas <pablo.dominguez@situm.com>
2020-12-31 17:23:46 +0000

    MMM?

--- db/seeds/test_seed.js
+++ db/seeds/test_seed.js
@@ -7,6 +7,14 @@ exports.seed = function (knex) {
         { id: 1, code: 'AAA' },
         { id: 2, code: 'BBB' },
         { id: 3, code: 'CCC' }
-      ]);
+      ]).then(() => knex('tracks').insert([
+        { id: 1, code: 'Ready to start' },
+        { id: 2, code: 'Besame mucho' },
+        { id: 3, code: 'Comfortably numb' }
+      ])).then(() => knex('session_tracks').insert([
+        { session_id: 1, track_id: 1 },
+        { session_id: 1, track_id: 2 },
+        { session_id: 1, track_id: 3 }
+      ]));
     });
 };
